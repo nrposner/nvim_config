05db1fa7a68cb6f37ca14ccc291ea13bdc93b713
nrposner <nicolasposner@gmail.com>
2025-05-22 11:16:25 -0500

    adding new keybinds

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+vim.lsp.get_active_clients = vim.lsp.get_clients
+
 vim.g.mapleader = " "
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
@@ -12,6 +14,12 @@ vim.cmd([[highlight LualineDir guifg=#808080]])
 
 
 vim.keymap.set("n", "<leader>e", ":lua vim.diagnostic.open_float()<CR>", {})
+
+vim.keymap.set("n", "<leader>n", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<leader>p", vim.diagnostic.goto_prev)
+vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = 'LSP Rename Symbol' })
+vim.keymap.set('n', '<leader>jl', "ggVG", { desc = "Select entire file" })
 
 vim.opt.number = true 
 vim.opt.wrap = true
@@ -46,5 +54,5 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("plugins")
 
 -- Map two consecutive spaces to Escape in Insert mode
-vim.keymap.set('i', '  ', '<Esc>', { noremap = true, silent = true })
+-- vim.keymap.set('i', '  ', '<Esc>', { noremap = true, silent = true })
 
