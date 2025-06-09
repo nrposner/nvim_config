0fa2966339f1b139bc1582489d04c5792fb12ecc
nrposner <nicolasposner@gmail.com>
2025-06-08 20:29:28 -0400

    adding shortcut for arrow

--- init.lua
+++ init.lua
@@ -9,6 +9,7 @@ vim.cmd("abb teh the")
 vim.cmd("abb hte the")
 vim.cmd("abb tostr to_string")
 vim.cmd("abb tostr() to_string()")
+vim.cmd("abb -- ->")
 vim.cmd([[highlight LualineFileNameBold gui=bold]])
 vim.cmd([[highlight LualineDir guifg=#808080]])
 
@@ -20,6 +21,8 @@ vim.keymap.set("n", "<leader>p", vim.diagnostic.goto_prev)
 vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
 vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = 'LSP Rename Symbol' })
 vim.keymap.set('n', '<leader>jl', "ggVG", { desc = "Select entire file" })
+
+-- vim.keymap.set('n', 'U', '<C-r>')
 
 vim.opt.number = true 
 vim.opt.wrap = true
@@ -53,6 +56,6 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
 
--- Map two consecutive spaces to Escape in Insert mode
--- vim.keymap.set('i', '  ', '<Esc>', { noremap = true, silent = true })
+-- Map two consecutive hs to Escape in Insert mode
+vim.keymap.set('i', 'hh', '<Esc>', { noremap = true, silent = true })
 
