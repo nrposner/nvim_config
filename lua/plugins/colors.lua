d2a55aa92f3f92dd1194801deaa996a036c42f7f
nrposner <nicolasposner@gmail.com>
2025-08-06 21:23:32 -0400

    updated colors

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,7 +1,21 @@
-return {"catppuccin/nvim", lazy = false, name = "catppuccin", priority = 1000, 
-	config = function()
-		vim.cmd.colorscheme "catppuccin"
-	end
+return {
+  "catppuccin/nvim",
+  lazy = false,
+  name = "catppuccin",
+  priority = 1000,
+  config = function()
+    require("catppuccin").setup({
+      transparent_background = true,
+      -- other options:
+      -- styles = {
+      --   comments = { "italic" },
+      --   conditionals = { "italic" },
+      -- },
+    })
+
+    -- Then, apply the colorscheme
+    vim.cmd.colorscheme("catppuccin")
+  end,
 }
 
 -- in case I decide to move back to tokyonight
