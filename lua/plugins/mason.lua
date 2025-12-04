76ed47b36dae51cfa171d1a23386a363a727a063
nrposner <nicolasposner@gmail.com>
2025-12-04 13:27:55 -0500

    fixing rust-analyzer duplicatino

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -12,16 +12,17 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
       require("mason-lspconfig").setup({
-        ensure_installed = { "rust_analyzer", "ty", "clangd", "wgsl_analyzer" },
+        ensure_installed = { "ty", "clangd", "wgsl_analyzer" },
+        -- ensure_installed = { "rust_analyzer", "ty", "clangd", "wgsl_analyzer" },
         automatic_installation = true,
         handlers = {
-          rust_analyzer = function() end,
+          -- rust_analyzer = function() end,
           ty = function()
             lspconfig.ty.setup({
               cmd       = { "ty", "lsp" },
               filetypes = { "python" },
               single_file_support = true,
-              settings  = { pyrefly = { typeCheckingMode = "strict" } },
+              -- settings  = { pyrefly = { typeCheckingMode = "strict" } },
             })
           end,
         },
